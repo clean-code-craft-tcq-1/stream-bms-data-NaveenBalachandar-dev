b83f14c2dbfd2f48b27e9b7b7799ef13595f309b
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-06-01 01:08:16 +0530

    Create BMS_Sender.h

---
+++ Sender/BMS_Sender.h
@@ -0,0 +1,12 @@
+/*****************************************************************************/
+/***    \file        BMS_Sender.h
+***     \author      Naveen Balachandar (RBEI/ECK3) 
+***
+***     \brief       File to monitor the battery condition based required properties
+/*****************************************************************************/
+
+
+/*Function to send ouput to console*/
+int readInpDataFile(void);
+/*Function to send ouput to console*/
+void sendDataToConsloe(float temp ,float chargeset);
