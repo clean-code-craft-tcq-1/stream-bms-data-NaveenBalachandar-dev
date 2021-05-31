a5bd8bf5c88b2d6799c4bc93e3aab5cb720693d6
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-05-31 19:04:38 +0530

    Create BMS_Sender.c

---
+++ sender/BMS_Sender.c
@@ -0,0 +1,9 @@
+/*****************************************************************************/
+/***    \file        BMS_Sender.c
+***     \author      Naveen Balachandar (RBEI/ECK3) 
+***
+***     \brief       File to monitor the battery condition based required properties
+/*****************************************************************************/
+/*------ standard includes -------*/
+
+#include <stdio.h>
