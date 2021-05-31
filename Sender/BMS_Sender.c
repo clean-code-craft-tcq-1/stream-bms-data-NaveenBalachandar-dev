e483d90d1e490e6debfd5b4ba7e546cf2c59a636
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-06-01 01:35:45 +0530

    Update BMS_Sender.c

--- Sender/BMS_Sender.c
+++ Sender/BMS_Sender.c
@@ -5,19 +5,43 @@
 ***     \brief       File to monitor the battery condition based required properties
 /*****************************************************************************/
 /*------ standard includes -------*/
-
 #include <stdio.h>
+#include <stdlib.h>
+
+/*------ Project includes -------*/
 #include "BMS_Sender.h"
+
+int readInpDataFile(void)
+{
+  
+FILE *fptr;
+float c;
+
+fptr=fopen("Inputdata.txt","r");
+if (fptr ! =NULL) 
+{
+ while ((c = getc(file)) != EOF)
+ {
+  putchar(c);
+ }       
+    fclose(file)
+}   
+   return 0;
+
+}  
+
 
 void sendDataToConsole(float temp ,float chargeRate)
 {
 printf("Temperature:%5.2f;chargeRate:%5.2f",temp,chargeRate);
 }
 
+
 int  main()
 {
   float temp = 11.5;
   float chargeRate = 15.5;
-  sendDataToConsloe(temp ,chargeRate);
+  readInpDataFile();
+  sendDataToConsole(temp ,chargeRate);
   return 0;
 }
