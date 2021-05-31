7b20a3dd5247209d93146884a121cf6569f9031b
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-05-31 21:57:35 +0530

    Create BMS_Sender.c

---
+++ Sender/BMS_Sender.c
@@ -0,0 +1,26 @@
+/*****************************************************************************/
+/***    \file        BMS_Sender.c
+***     \author      Naveen Balachandar (RBEI/ECK3) 
+***
+***     \brief       File to monitor the battery condition based required properties
+/*****************************************************************************/
+/*------ standard includes -------*/
+
+#include <stdio.h>
+
+/*Function to send ouput to console*/
+static void SendToConsloe(float temp ,float chargeset);
+
+
+static void SendToConsloe(float temp ,float chargeset)
+{
+printf("Yet to be printed");
+}
+
+int  main()
+{
+  float temp = 11.5;
+  float chargeset = 15.5;
+  SendToConsloe(temp ,chargeset);
+  return 0;
+}
