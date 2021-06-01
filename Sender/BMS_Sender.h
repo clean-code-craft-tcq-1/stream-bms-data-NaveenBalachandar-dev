a7cfdf16cbfcf54cdd1acf7294bf884d9507a6d1
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-06-01 15:57:51 +0530

    Update BMS_Sender.h

--- Sender/BMS_Sender.h
+++ Sender/BMS_Sender.h
@@ -2,9 +2,24 @@
 /***    \file        BMS_Sender.h
 ***     \author      Naveen Balachandar (RBEI/ECK3) 
 ***
-***     \brief       File to monitor the battery condition based required properties
+***     \brief       Header for BMS parameter streaming project
 /*****************************************************************************/
 
+/*enums used forUnit testing*/
+typedef enum {
+ SENT_TO_CONSOLE,
+ NOT_SENT
+}ConsoleSentStatus_t;
+
+typedef enum {
+ FILE_ACCESS_SUCCESS,
+ FILE_ACCESS_FAILURE
+}FileReadStatus_t;
+
+typedef enum {
+ DATA_MATCHING,
+ DATA_NOT_MATCHING
+}DataValidStatus_t;
 
 /*Function to send ouput to console*/
 int readInpDataFile(void);
