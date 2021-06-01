26b78b7cf6d36ab30f4d95920561a4779efe498e
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-06-01 20:36:41 +0530

    Update BMS_sendertest.cpp

--- Sender/BMS_sendertest.cpp
+++ Sender/BMS_sendertest.cpp
@@ -7,7 +7,7 @@ TEST_CASE("validate that O/P fed to console") {
 	
 	Alert_Status_s AlertTestStat = {NOT_SENT,FILE_ACCESS_FAILURE};
   
-	AlertStatRest = sendDataToConsole(11.5, 0.7);
+	AlertTestStat = sendDataToConsole(11.5, 0.7);
 	
   REQUIRE(AlertTestStat.ConsoleSentStatus   == SENT_TO_CONSOLE);
 
