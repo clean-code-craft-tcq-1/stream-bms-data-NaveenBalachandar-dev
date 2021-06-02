4cc9ed173352f550de955fd234b7160388541f7f
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-06-02 23:23:33 +0530

    Create BMS_sendertest.cpp

--- Sender/BMS_sendertest.cpp
+++ Sender/BMS_sendertest.cpp
@@ -1,22 +1,33 @@
+/*--------------------------------------------------------------------------------
+---    \file       BMS_sendertest.cpp
+---   \author      Naveen Balachandar (RBEI/ECK3) 
+---
+---     \brief     unit testing file to test streaming BMS data
+------------------------------------------------------------------------------------*/
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "../test/catch.hpp"
 #include "BMS_Sender.h"
 
-TEST_CASE("validate that O/P fed to console") 
+TEST_CASE("Test case 1 : Console output sent status") 
 {
+    /*Inital conditions*/	
     streamAlert_Status_s streamAlertTestStat = {NOT_SENT,FILE_ACCESS_FAILURE};
+    /*func call with stub data*/
     streamAlertTestStat = sendDataToConsole(11.5, 0.7);
-	
-    REQUIRE(streamAlertTestStat.ConsoleSentStatus   == SENT_TO_CONSOLE);
+ 
+    /*Validation success*/	  
+    REQUIRE(streamAlertTestStat.ConsoleSentStatus == SENT_TO_CONSOLE);
 
 }
 
-TEST_CASE("validate that file read and stream data with invalid file") 
+TEST_CASE("Test case 2 : validate file read and stream data with invalid  path") 
 {
+    /*Inital conditions*/	
     streamAlert_Status_s streamAlertTestStat = {SENT_TO_CONSOLE,FILE_ACCESS_SUCCESS};
-    char testFilePath[50] = "data.txt"; /*invalid file */
- 
+    /*stub data*/ 	
+    char testFilePath[50] = "Inputdata.txt";
+    /*func call with invalid file path*/
     streamAlertTestStat = streamFileInpData(testFilePath);
 	
      REQUIRE(streamAlertTestStat.FileReadStatus   == FILE_ACCESS_FAILURE);
@@ -24,15 +35,30 @@ TEST_CASE("validate that file read and stream data with invalid file")
 
 }
 
-TEST_CASE("validate that file read and stream data with valid path") 
+TEST_CASE("Test case 3 : validate file read and stream data with invalid file name") 
 {
-    streamAlert_Status_s streamAlertTestStat = {NOT_SENT,FILE_ACCESS_FAILURE};
-    char testFilePath[50] = "Sender/Inputdata.txt";
- 
-     streamAlertTestStat = streamFileInpData(testFilePath);
+    /*Inital conditions*/	
+    streamAlert_Status_s streamAlertTestStat = {SENT_TO_CONSOLE,FILE_ACCESS_SUCCESS};
+    /*stub data*/ 	
+    char testFilePath[50] = "Sender/data.txt";
+    /*func call with invalid file name*/
+    streamAlertTestStat = streamFileInpData(testFilePath);
 	
-     REQUIRE(streamAlertTestStat.FileReadStatus   == FILE_ACCESS_SUCCESS);
-     REQUIRE(streamAlertTestStat.ConsoleSentStatus   == SENT_TO_CONSOLE);
+     REQUIRE(streamAlertTestStat.FileReadStatus   == FILE_ACCESS_FAILURE);
+     REQUIRE(streamAlertTestStat.ConsoleSentStatus   == NOT_SENT);
+}
 
+
+TEST_CASE("Test case 4 : validate that file read and stream data with valid file") 
+{
+    /*Inital conditions*/	
+    streamAlert_Status_s streamAlertTestStat = {NOT_SENT,FILE_ACCESS_FAILURE};
+    /*stub data*/ 	
+    char testFilePath[50] = "Sender/Inputdata.txt";
+    /*func call with valid file name*/
+    streamAlertTestStat = streamFileInpData(testFilePath);
+	
+    REQUIRE(streamAlertTestStat.FileReadStatus   == FILE_ACCESS_SUCCESS);
+    REQUIRE(streamAlertTestStat.ConsoleSentStatus   == SENT_TO_CONSOLE);
 }
 
