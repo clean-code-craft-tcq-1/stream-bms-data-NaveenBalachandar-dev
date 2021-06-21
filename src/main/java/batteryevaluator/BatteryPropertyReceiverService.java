59a8425bff8799f276c26fdad72b3ba16c095048
Sushmitha Rajendran <rajendran.sushmitha@in.bosch.com>
2021-06-21 17:00:03 +0530

    Changed to Buffer reader

--- src/main/java/batteryevaluator/BatteryPropertyReceiverService.java
+++ src/main/java/batteryevaluator/BatteryPropertyReceiverService.java
@@ -1,58 +1,37 @@
 package batteryevaluator;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import batteryproperties.Battery;
 import batteryevaluatorreporter.ConsolePropertyReporter;
 import batteryevaluatorreporter.PropertyReporter;
 
 public class BatteryPropertyReceiverService {
-	public static void main(String args[]) {
-		System.out.println("I am gere!" + args.length);
-	
+	public static void main(String args[]) throws NumberFormatException, IOException {
 		List<Float> socList = new ArrayList<Float>();
 		List<Float> chargeRateList = new ArrayList<Float>();
 		Battery battery = new Battery();
-        BatteryPropertyEvaluator propertyEvaluator = new BatteryPropertyEvaluator();
-		
-		 try {
-            // Wrap the System.in inside BufferedReader
-            // But do not close it in a finally block, as we 
-            // did no open System.in; enforcing the rule that
-            // he who opens it, closes it; leave the closing to the OS.
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            String line;
-            while ((line = in.readLine()) != null) {
-                // TODO: Handle input line
-		    System.out.println(line);
-            }
+		BatteryPropertyEvaluator propertyEvaluator = new BatteryPropertyEvaluator();
 
-            // Null was received, so loop was aborted.
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+		while ((line = in.readLine()) != null) {
+			if (line.contains(";")) {
+				String batteryProperties[] = line.split("; ");
+				socList.add(Float.valueOf(batteryProperties[0]));
+				chargeRateList.add(Float.valueOf(batteryProperties[1]));
+			}
+		}
+		evaluateBatteryChargeRate(chargeRateList, battery, propertyEvaluator);
+		evaluateBatterySoc(socList, battery, propertyEvaluator);
 
-        } catch (IOException e) {
-            // TODO: Add error handler
-        }
-        
-		 if (args.length > 0) {       
-			 	for (String consoleInput : args) {
-					System.out.println("I am again here!" + consoleInput);
-					String batteryProperties[] = consoleInput.split("; ");
-					socList.add(Float.valueOf(batteryProperties[0]));
-					chargeRateList.add(Float.valueOf(batteryProperties[1]));
-				}      
-			 
-	            evaluateBatteryChargeRate(chargeRateList, battery, propertyEvaluator);
-	            evaluateBatterySoc(socList, battery, propertyEvaluator);
-	            
-	            PropertyReporter consoleReporter = new ConsolePropertyReporter();
-	            consoleReporter.printReport(battery);
-	            
-	        }
-	          
+		PropertyReporter consoleReporter = new ConsolePropertyReporter();
+		consoleReporter.printReport(battery);
+
 	}
 
 	private static void evaluateBatterySoc(List<Float> socList, Battery battery,
